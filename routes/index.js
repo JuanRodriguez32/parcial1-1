2d096d551cb9dfa9981310fff1b795d82fbe50b6
dalazaca <diezucchet@gmail.com>
2017-09-21 09:22:10 -0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+var GitHubApi = require("github");
+  
+// TODO: optional authentication here depending on desired endpoints. See below in README.
+router.get('/getFollowers/:user', function(req, res, next) {
+	
+	var github = new GitHubApi({}); 
+	
+	github.users.getFollowingForUser({
+    	username: req.params.user
+	}, function(err, data) {
+    	res.json(data)
+	});
+});
+
+
+
+module.exports = router;
