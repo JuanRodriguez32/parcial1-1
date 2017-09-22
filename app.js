a5b257e269ee3d4b7c15ff95ec4eab86eb4e6fcb
dalazaca <diezucchet@gmail.com>
2017-09-22 11:53:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -47,4 +47,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+const port = process.env.PORT || 3001;
+app.listen(port);
+
 module.exports = app;
